30cc92bd217d53b1aae6e6764c92ce29fbd9855b
alenzer <alenzer0902@gmail.com>
2022-12-15 12:58:26 -0500

    AddressPriority

--- contracts/contracts/feature/src/state.rs
+++ contracts/contracts/feature/src/state.rs
@@ -4,10 +4,12 @@ use serde::{Deserialize, Serialize};
 use cosmwasm_std::{Addr};
 use cw_storage_plus::Item;
 
+use crate::msg::AddressPriority;
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
     pub owner: Addr,
-    pub list: Vec<Addr>
+    pub list: Vec<AddressPriority>
 }
 
 pub const STATE: Item<State> = Item::new("state");
